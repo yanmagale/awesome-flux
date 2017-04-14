604bc829c646125aeb1e2306e66fe15583764218
= <yanmagale@gmail.com>
2017-04-14 08:52:09 -0300

    Change allow permitions in ci

--- ci_script.sh
+++ ci_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec awesome_bot README.md --allow 301,302 --set-timeout 10 -w code-cartoons
+bundle exec awesome_bot README.md --allow 302,429 --allow-redirect -w code-cartoons,medium
