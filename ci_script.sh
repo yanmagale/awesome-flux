f92856d9d9e2b7631ca1cc4460e9eb71eedf3131
= <yanmagale@gmail.com>
2017-04-14 08:21:19 -0300

    Added links about code cartoons and awesome redux

--- ci_script.sh
+++ ci_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec awesome_bot README.md --allow-redirect --set-timeout 10
+bundle exec awesome_bot README.md --allow-redirect --set-timeout 10 --white-list https://code-cartoons.com/a-cartoon-guide-to-flux-6157355ab207 https://code-cartoons.com/hot-reloading-and-time-travel-debugging-what-are-they-3c8ed2812f35
