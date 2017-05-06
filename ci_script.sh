05fcd179f3c548c501656701d76e18b0c6e8025a
= <yanmagale@gmail.com>
2017-05-06 11:32:42 -0300

    Update rules awesome bot

--- ci_script.sh
+++ ci_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec awesome_bot README.md --allow 301,302,429 --allow-redirect -w code-cartoons,medium
+bundle exec awesome_bot README.md --allow 301,302,429 --allow-redirect --white-list code-cartoons.com,medium.com
